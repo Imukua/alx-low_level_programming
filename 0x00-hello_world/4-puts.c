ba17b470b7eb7ac53fe4151fc3464c463232d980
Imukua <ianmukuaa@gmail.com>
2023-02-16 13:16:17 +0300

    shell_redirections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 
 int main() {
+    puts("\"Programming is like building a multilingual puzzle");
 
-puts("\"Programming is like building a multilingual puzzle");
-
-return 0;
+    
+  return 0;
 
 }
