cd64870757845ec15822c76ca4869050d00e20f3
Imukua <ianmukuaa@gmail.com>
2023-02-16 16:09:32 +0300

    shell_redirections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message[] = "and that piece of art is useful\""
+		" - Dora Korpar, 2015-10-19\n";
 	const int length = sizeof(message) - 1;
 	const int fd = 2;
 
 	write(fd, message, length);
-
-	 return (1);
+	return (1);
 }
