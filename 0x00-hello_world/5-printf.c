05c89e24b11d50ed5fe93718d4bb22ef2d7cf93b
Imukua <ianmukuaa@gmail.com>
2023-02-16 14:36:27 +0300

    shell_redirections

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *Main function : printing
+ *
+ *Return 0 : success
+ */
+int main()
+{
+   Printf("with proper grammar, but the outcome is a piece of art,\n");
+   return 0;
+}
