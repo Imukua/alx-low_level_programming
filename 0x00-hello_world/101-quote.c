2982da1a7ce35d4ea3c0886dca201c5f0d8aea34
Imukua <ianmukuaa@gmail.com>
2023-02-16 16:06:08 +0300

    shell_redirections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 /**
- *main - Entry poin
+ *main - entry point of the program
  *
- *Return: Always 1 (Success)
+ *Return: Always 1 (error)
  */
 int main(void)
 {
 	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	const int length = sizeof(message) - 1;
 	const int fd = 2;
-	
+
 	write(fd, message, length);
-	return(1);
+
+	 return (1);
 }
