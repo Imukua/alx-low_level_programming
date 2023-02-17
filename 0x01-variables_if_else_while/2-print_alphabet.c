a2b2375dd040d3b0da23f96bf2c832143347e6a3
Imukua <ianmukuaa@gmail.com>
2023-02-17 17:57:04 +0300

    shell_redirections

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: always success
+ */
+int main(void)
+{
+	for (char x = 'a';X <= 'z';X++)
+		putchar(X);
+	putchar('\n')
+	return (0);
+}
