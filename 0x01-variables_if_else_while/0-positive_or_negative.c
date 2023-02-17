9301b5718134b91bab6732edd44f922b06d8df23
Imukua <ianmukuaa@gmail.com>
2023-02-17 15:49:58 +0300

    shell_redirections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,18 +14,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0) 
 	{
-		printf("%d is Positive", n);
+		printf("%d is Positive\n", n);
 
 	}
 
-	else if (n==0)
+	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	
 	}
-	else if (n<0)
+	else if (n < 0)
 	{
-		printf("%d is Negative", n);
+		printf("%d is Negative\n", n);
 	
 	}
 	
