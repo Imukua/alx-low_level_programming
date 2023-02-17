45ffa24e2ae2abccd1bf95e5b0b03f2168b1524c
Imukua <ianmukuaa@gmail.com>
2023-02-17 19:36:35 +0300

    shell_redirections

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: always success
+ */
+int main(void)
+{
+	int X;
+
+	for (X = 0; X <= 9; X++)
+	{
+		putchar(X);
+	}
+
+	putchar('\n');
+	return (0);
+}
