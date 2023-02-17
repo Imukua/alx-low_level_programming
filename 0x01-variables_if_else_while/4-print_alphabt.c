826ed559672ffc86e5fb8187cb4335f1ffa95496
Imukua <ianmukuaa@gmail.com>
2023-02-17 19:26:56 +0300

    shell_redirections

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,11 +3,11 @@
  *  * main - Main function
  *   *
  *    * Return: always success
- *     */
+ *
+ */
 int main(void)
 {
 	char X;
-	
 	for (X = 'a'; X <= 'z'; X++)
 	{
 		if (X != 'e' && X != 'q')
