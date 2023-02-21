e5d1fc619de20293297588b01f31cd8a495b4375
Imukua <ianmukuaa@gmail.com>
2023-02-21 15:34:32 +0300

    shell_redirections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/*
-*print_alphabet - print the alphabet in lower case
-*Return: always 0
-*/
+/**
+ * print_alphabet - print the alphabet in lower case
+ * Return: 0 (Success)
+ */
 void print_alphabet(void)
 {
 	int i;
