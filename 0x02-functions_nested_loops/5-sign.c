2619e27160de4a4290ad4dc836451ead78343e1d
Imukua <ianmukuaa@gmail.com>
2023-02-21 17:05:07 +0300

    shell_redirections

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  */
 
 int print_sign(int n)
-{	
-	if (n > 0 )
+{
+	if (n > 0)
 	{	
 		_putchar('+');
 		return (1);
@@ -18,7 +18,7 @@ int print_sign(int n)
 		return (-1);
 	}
 	else if (n==0)
-	{		
+	{
 		_putchar('0');
 	}
 		return (0);
