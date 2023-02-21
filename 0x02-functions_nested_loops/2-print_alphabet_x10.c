5b83f2f4dbf3aa3d2897980e3b9222d23cfd95e8
Imukua <ianmukuaa@gmail.com>
2023-02-21 15:56:30 +0300

    shell_redirections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 /**
  *  * print_alphabet_x10 - prints 10 times the alphabet,
  *   *  in lowercase, followed by a new line
- *    */
-void print_alphabet_x10 (void)
+ */
+void print_alphabet_x10(void)
 {	int i;
 	int times;
 
 	for (times = 0; times < 10; times++)
 	{
-		for(i = 'a'; i <= 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			putchar(i);
 		}
@@ -18,4 +18,3 @@ void print_alphabet_x10 (void)
 	}
 
 }
-		
