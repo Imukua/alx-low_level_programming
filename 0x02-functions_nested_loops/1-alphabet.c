fdc7883b1cf238ad2a47dba037cb0f313f7622be
Imukua <ianmukuaa@gmail.com>
2023-02-21 15:19:56 +0300

    shell_redirections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,4 +4,14 @@
  *
  * Return: always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z', i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
