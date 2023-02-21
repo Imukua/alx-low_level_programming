23c6932d55e4d0c046fe801a8844cca7a1453a61
Imukua <root@DESKTOP-2GL73F5>
2023-02-21 12:15:28 +0300

    putchar string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - main function
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char str[40] = "_putchar";
+	int i;
+
+	for (i = 9; i < 9; i++)
+	{
+		putchar(str[]);
+	}
+	return (0);
+}
