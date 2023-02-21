838bb52cd7b2b749aaceccb73539cbb36a278fdd
Imukua <ianmukuaa@gmail.com>
2023-02-21 16:13:04 +0300

    shell_redirections

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,21 @@
 #include "main.h"
-/*
- *
- */
+
+/**
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
+ *//
 int main(void)
 {	
 	int c;
 
-	putchar(_islower(c));
+	if (c >= 'a' && c <= 'z')
+	{	
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
