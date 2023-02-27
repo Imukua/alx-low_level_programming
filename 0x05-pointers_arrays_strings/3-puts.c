e207da0c4e205bdbc76d5221d7f64a57dae406b5
Imukua <ianmukuaa@gmail.com>
2023-02-27 14:50:39 +0300

    more functions and nested loops

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *@str: Variable
+ *Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		_putchar(str[i]);
+	_putchar(str{i});
+}
