0f1c3eab3cbc87939267fe262f41568079a1b29b
Imukua <ianmukuaa@gmail.com>
2023-02-27 15:29:13 +0300

    more functions and nested loops

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/*
- * print_rev -  prints a string, in reverse
+/**
+ *print_rev -  prints a string, in reverse
  *@s: Variable
  *Return: void
  */
@@ -9,7 +9,7 @@ void print_rev(char *s)
 	int i;
 	int len = 0;
 
-	for (i = 0; s[i] != '\0';i++)
+	for (i = 0; s[i] != '\0'; i++)
 		len++;
 	len = len - 1;
 	while (len >= 0)
