d47d4b01fc80c14f2f3493fdfd0672ac81a969c0
Imukua <ianmukuaa@gmail.com>
2023-02-27 14:36:11 +0300

    more functions and nested loops

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,16 @@
 #include "main.h"
 int _strlen(char *s)
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to find the length of
+ *
+ * Return: the length of the string
+ */
 {
 	int i;
 	int counter = 0;
 
-	for (i = 0; i <= *s; i++)
+	for (i = 0; i != '\0'; i++)
 		counter++;
 	return (counter);
 }
