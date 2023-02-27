939920d3374ae89d2b7e2991845fbc8448678f1f
Imukua <ianmukuaa@gmail.com>
2023-02-27 15:16:24 +0300

    more functions and nested loops

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ * print_rev -  prints a string, in reverse
+ *@s: Variable
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j;
+	int len;
+
+	for (i = 0; s[i] != '\0';i++)
+		len++;
+	while (s[len] >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+}
+
+
+
