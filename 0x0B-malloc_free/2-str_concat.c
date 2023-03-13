47d75a8ac9b97ca674e971c109443d4c6c016251
Imukua <ianmukuaa@gmail.com>
2023-03-13 05:06:16 -0700

    more functions and nested loops

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,8 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-	{	if (s2 == NULL)
+	if (s2 == NULL)
+	{	
 		s2 = "";
 	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
