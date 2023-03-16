5c6c0cbec523bbaa45828df5b47406af5dc3aee3
Imukua <ianmukuaa@gmail.com>
2023-03-16 03:47:56 -0700

    more functions and nested loops

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,34 +8,34 @@
  * @n: number of bytes from s2 to concatenate to s1
  *
  * Return: pointer to the resulting string
- */
+ * /
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    result = malloc(sizeof(char) * (len1 + n + 1));
-    if (result == NULL)
-        return (NULL);
+	result = malloc(sizeof(char) * (len1 + n + 1));
+	if (result == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        result[i] = s1[i];
-    for (j = 0; j < n; j++)
-        result[i + j] = s2[j];
-    result[i + j] = '\0';
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
+	result[i + j] = '\0';
 
-    return (result);
+	return (result);
 }
