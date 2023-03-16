7341b9afd9ad747214ad3d8f407e58c0b183782c
Imukua <ianmukuaa@gmail.com>
2023-03-16 04:06:16 -0700

    more functions and nested loops

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,27 +2,26 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
+ * *array_range - fills memory with a constant byte
+ * @min: memory area to be filled
+ * @max: char to copy
  *
  * Return: pointer to the memory area s
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int i, size;
+	int *arr;
+	int i, size;
 
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    size = max - min + 1;
-    arr = malloc(size * sizeof(int));
-    if (arr == NULL)
-        return (NULL);
+	size = max - min + 1;
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = min++;
-
-    return (arr);
+	for (i = 0; i < size; i++)
+		arr[i] = min++;
+	return (arr);
 }
