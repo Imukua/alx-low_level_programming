2e829db40ee0180d020082d6cd42d2d1148ef896
Imukua <ianmukuaa@gmail.com>
2023-04-06 13:57:52 +0300

    Binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * binary_to_uint - Function to convert binary to unsigned int
+ * @b: const char variable
+ * Return: result returned
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sol = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+			sol = (sol << 1) + 1;
+		else if (b[i]  == '0')
+			sol = sol << 1;
+		else
+			return 0;
+	}
+	return (result);
+}
