2917eac906e5853b4321df3f61754ac79cc924e2
Imukua <ianmukuaa@gmail.com>
2023-04-06 14:27:22 +0300

    bINARY TO UNSIGNED INT

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sol = 0;
-	int i = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
-	for (; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1')
 			sol = (sol << 1) + 1;
 		else if (b[i]  == '0')
 			sol = sol << 1;
 		else
-			return 0;
+			return (0);
 	}
-	return sol;
+	return (sol);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
 #endif
