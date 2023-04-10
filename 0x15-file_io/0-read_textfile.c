61fc64839e2926a67f860448acefe7148ce688d4
Imukua <ianmukuaa@gmail.com>
2023-04-10 05:07:51 -0700

    file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
@@ -7,42 +8,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int feud, soma, andika;
-  char *buffee;
+	int feud, soma, andika;
+	char *buffee;
 
-  if (filename == NULL)
-    return (0);
+	if (filename == NULL)
+	return (0);
 
-  buffee = malloc(sizeof(char) * letters);
-  if (buffee == NULL)
-    return (0);
+	buffee = malloc(sizeof(char) * letters);
+	if (buffee == NULL)
+	return (0);
 
-  feud = open(filename, O_RDONLY);
-  if (feud == -1)
-  {
-    free(buffee);
-    return (0);
-  }
+	feud = open(filename, O_RDONLY);
+	if (feud == -1)
+	{
+	free(buffee);
+	return (0);
+	}
 
-  soma = read(feud, buffee, letters);
-  if (soma == -1)
-  {
-    free(buffee);
-    close(feud);
-    return (0);
-  }
+	soma = read(feud, buffee, letters);
+	if (soma == -1)
+	{
+	free(buffee);
+	close(feud);
+	return (0);
+	}
 
-  andika = write(STDOUT_FILENO, buffee, soma);
-  if (andika == -1 || andika != soma)
-  {
-    free(buffee);
-    close(feud);
-    return (0);
-  }
+	andika = write(STDOUT_FILENO, buffee, soma);
+	if (andika == -1 || andika != soma)
+	{
+	free(buffee);
+	close(feud);
+	return (0);
+	}
 
-  free(buffee);
-  close(feud);
+	free(buffee);
+	close(feud);
 
-  return (andika);
+	return (andika);
 }
 
