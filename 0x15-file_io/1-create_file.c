8b5e26820b9c642148510d0f5c924b8820459da4
Imukua <ianmukuaa@gmail.com>
2023-04-10 05:43:28 -0700

    file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,34 @@
-#include "main.h"
-
 /**
- * create_file - creates a file and writes a string to it
- * @filename: the name of the file to create
- * @text_content: the string to write to the file
+ * create_file - creates a file and writes text content to it
+ * @filename: name of the file to create
+ * @text_content: string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0, wrote = 0;
+    int file_de, return_v;
+    ssize_t size = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+    file_de = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+    if (file_de == -1)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
+    if (text_content != NULL)
+    {
+        while (text_content[size] != '\0')
+            size++;
 
-		wrote = write(fd, text_content, len);
-		if (wrote == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+        return_v = write(file_de, text_content, size);
+        if (return_v == -1)
+            return (-1);
+    }
 
-	close(fd);
-	return (1);
+    close(file_de);
+
+    return (1);
 }
 
