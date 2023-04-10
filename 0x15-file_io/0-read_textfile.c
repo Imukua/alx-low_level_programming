41a06c86629d677f1185f7f8a5ac1f496dd300da
Imukua <ianmukuaa@gmail.com>
2023-04-10 05:29:23 -0700

    file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,49 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to stdout
- * @filename: pointer to the name of the file to read
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
  * @letters: number of letters to read and print
- *
- * Return: number of letters read and printed, or 0 on failure
+ * Return: actual number of letters read and printed, or 0 if error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t feud, soma, andika;
-    char *buffee;
-
-    buffee = malloc(letters);
-    if (buffee == NULL)
-        return (0);
+    int fd;
+    ssize_t num_read, num_written;
+    char *buf;
 
     if (filename == NULL)
         return (0);
 
-    feud = open(filename, O_RDONLY);
-    if (feud == -1)
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    buf = malloc(sizeof(char) * letters);
+    if (buf == NULL)
     {
-        free(buffee);
+        close(fd);
         return (0);
     }
 
-    soma = read(feud, buffee, letters);
-
-    andika = write(STDOUT_FILENO, buffee, soma);
-
-    close(feud);
-    free(buffee);
-
-    if (andika != soma)
+    num_read = read(fd, buf, letters);
+    if (num_read == -1)
+    {
+        free(buf);
+        close(fd);
         return (0);
+    }
 
-    return (andika);
+    num_written = write(STDOUT_FILENO, buf, num_read);
+    if (num_written == -1 || num_written != num_read)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
+
+    free(buf);
+    close(fd);
+    return (num_written);
 }
 
