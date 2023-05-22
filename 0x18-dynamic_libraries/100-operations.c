792031d51f2dfcc4b86f4c432bcff01945263e2d
Imukua <ianmukuaa@gmail.com>
2023-05-22 04:16:32 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
+int add(int a, int b)
+{
+    return a + b;
+}
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference between the two integers.
+ */
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of the two integers.
+ */
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+/**
+ * div - Divides two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the division.
+ */
+int div(int a, int b)
+{
+    return a / b;
+}
+
+/**
+ * mod - Computes the modulus of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The modulus of the two integers.
+ */
+int mod(int a, int b)
+{
+    return a % b;
+}
+
