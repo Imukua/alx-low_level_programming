1180e89680013aaa110c04ec292687a3c7c1cfd7
Imukua <ianmukuaa@gmail.com>
2023-06-09 13:26:51 +0300

    header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,22 +2,24 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
-#include <string.h>
-#include <stdlib.h>
+
+/* Structures */
 
 /**
  * struct hash_node_s - Node of a hash table
  *
- * @key: The key, string
+ * @key: The key (string)
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * @next: A pointer to the next node
+ *
+ * Description: Hash node structure
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -25,25 +27,36 @@ typedef struct hash_node_s
  *
  * @size: The size of the array
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
+ * Each cell of this array is a pointer to the first node of a linked list
  * because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
-/* Function to create a hash table */
+/* Function Prototypes */
+
+/* 0-hash_table_create.c */
 hash_table_t *hash_table_create(unsigned long int size);
 
-/* Hash function: djb2 algorithm */
+/* 1-djb2.c */
 unsigned long int hash_djb2(const unsigned char *str);
 
-/* Function to get the index of a key */
+/* 2-key_index.c */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
-/* Function to add an element to the hash table */
+/* 3-hash_table_set.c */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+/* 4-hash_table_get.c */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
+/* 5-hash_table_print.c */
+void hash_table_print(const hash_table_t *ht);
+
+/* 6-hash_table_delete.c */
+void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
